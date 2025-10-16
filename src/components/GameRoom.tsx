b355afae6e03dccbe70295028459d4655c4cfbd7
danplenicianu-dot <dan.plenicianu@gmail.com>
2025-10-16 06:25:55 -0700

    Create GameRoom.tsx

---
+++ src/components/GameRoom.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+export default function GameRoom() {
+  return (
+    <div className="p-4">
+      <h2 className="text-xl mb-2">Camera de joc</h2>
+      <p>Componentele jocului vor fi implementate aici.</p>
+    </div>
+  );
+}
